e0537c2982f0d7851d785f3235e7de9da16c6624
31DarkWolf31 <polcerstas117@gmail.com>
2023-03-20 11:19:36 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y, p;
+cout << "Калькулятор" << endl;
+cout << "enter number: " << endl;
+cin >> x;
+cout << "enter number: " << endl;
+cin >> y;
+cout << "enter number: " << endl;
+cin >> p;
+cout << "сума= " << x*y*p << endl;
+cout << "добуток= " << x+y+p << endl;
+return 0;
+}
